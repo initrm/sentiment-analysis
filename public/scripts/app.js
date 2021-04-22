85f22edd9601b933928555a3437dc464e7ba965f
Andrea Tinelli <tinelliandrea2@gmail.com>
2021-04-22 18:47:11 +0200

    Major changes: separated Tweets class from app.js file into is own file (tweets.js), fixed some bugs and started to implement the guided training logic

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,86 +1,6 @@
 /**
- * OVERVIEW: Tweets Class provides useful methods to iteract with the tweets api
+ * Functions
  */
-class Tweets {
-
-    /**
-     * Checks if there are some cached tweets and checks the data integrity
-     * 
-     * @returns {Boolean}
-     */
-    static #areCached = () => {
-
-        // Check if cache contains tweets list data
-        let tweets = localStorage.getItem('tweets');
-
-        // Checks the integrity of the data
-        try {
-            var tweetsArray = JSON.parse(tweets);
-        }
-        catch(error) {
-            return false;
-        }
-        if(!tweetsArray || tweetsArray.some(({ created_at, text, author_id, id }) => {
-            return typeof(created_at) !== typeof(text) || typeof(text) !== typeof(author_id) || typeof(author_id) !== typeof(id) || typeof(id) !== 'string'
-        })) return false;
-
-        return true;
-
-    }
-
-    /**
-     * Store into the cache the array of tweets
-     * 
-     * @param {Array} tweets
-     */
-    static #cache = (tweets) => localStorage.setItem('tweets', JSON.stringify(tweets))
-
-    /**
-     * Returns an array of tweets which can come from the cache or from the api
-     * 
-     * @returns {Array}
-     */
-    static #getTweets = async () => {
-
-        return new Promise((resolve, reject) => {
-
-            // Checks if there are at least one tweet cached, in that case it returns the cached tweets
-            if(Tweets.#areCached() && JSON.parse(localStorage.getItem('tweets')).length > 0) 
-                return resolve({ tweets: JSON.parse(localStorage.getItem('tweets')), source: 'cache' })
-    
-            // No tweets cached, retrieves them from the api
-            fetch('/api/tweets')
-                // Converts the response into a JSON object
-                .then((response) => response.json())
-                .then((data) => {
-                    // Returns the tweets list
-                    resolve({ tweets: data, source: 'api' })
-                })
-                // Returns the error
-                .catch((error) => reject(error));
-            
-        });
-
-    }
-
-    /**
-     * Returns the first tweet available and updates the cache removing the returned tweet
-     * 
-     * @returns {Object}
-     */
-    static pickOne = async () => {
-
-        let tweets = (await Tweets.#getTweets()).tweets
-
-        // Takes the tweets and caches the other ones
-        let tweet = tweets[0]
-        Tweets.#cache(tweets.slice(1))
-
-        return tweet
-        
-    }
-
-}
 
 /**
  * Hides the loading screen div
@@ -111,6 +31,198 @@ const displayTweet = (parentId, { id: tweetId, text, author_id, created_at }) =>
 
 }
 
+/**
+ * Initializes the Simple Training tab
+ */
+const initSimpleTraining = () => {
+
+    // Gets the tweet and shows it into the Simple Training tweet container
+    Tweets.pickOne()
+        .then((tweet) => displayTweet('st-tweet-container', tweet))
+        .catch((error) => toastr.error(error.message))
+        .finally(() => hideAppLoadingScreen());
+
+    // Sets the skip tweet classification button functionality
+    $('#st-skip-tweet-classification-button').click(function(event) {
+
+        // Disabling the button while it loads the next tweet
+        $(this).attr('disabled', 'true');
+        $(this).html("<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>");
+
+        // Gets the tweet
+        Tweets.pickOne()
+            // Sets the tweet into the page
+            .then((tweet) => displayTweet('st-tweet-container', tweet))
+            // Toast the error
+            .catch((error) => toastr.error(error.message))
+            // Reset the skip button to be usable and resets the selected text input textarea
+            .finally(() => {
+                $(this).removeAttr('disabled');
+                $(this).html("<i class=\"fas fa-forward\"></i> Skip");
+                $("textarea[name=st-selected-text]").val('');
+            });
+
+    });
+
+    // Sets the classification form behavior
+    $('#st-classification-form').on('submit', function(event) {
+
+        // Prevents the form to be submitted in the "standard" way
+        event.preventDefault();
+
+        // Sets the submit button on loading
+        let classificationSubmitButton = $('#st-classification-form-submit-button');
+        classificationSubmitButton.attr('disabled', 'true');
+        classificationSubmitButton.html("<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>");
+
+        // Sends the request to the
+        fetch('/api/classifier/learn', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ text: $("textarea[name=st-selected-text]").val(), category: $("input[name=st-radio-classification]:checked").val() })
+        })
+            // Computes the response
+            .then(async (response) => {
+                // Reads the response content as a JSON
+                let data = await response.json();
+                // Checks if the request has been completed without errors
+                if(response.status >= 300) return toastr.error(data.message);
+                // Notifies the user, resets the form and goes to the next tweet
+                toastr.success(data.message);
+                $('#st-skip-tweet-classification-button').trigger('click');
+                $(this).trigger('reset');
+            })
+            // Error Handling
+            .catch((error) => toastr.error("Unknown error."))
+            .finally(() => {
+                classificationSubmitButton.removeAttr('disabled');
+                classificationSubmitButton.html("Submit");
+            });
+
+    });
+
+    // Sets the listener on text selection inside the tweet card
+    document.addEventListener('selectionchange', () => {
+
+        // Sets the tweet selected text into the selected text input
+        if(window.getSelection && window.getSelection().anchorNode && $(window.getSelection().anchorNode.parentElement).attr('id') === 'st-tweet-text') {
+            let val = window.getSelection().toString();
+            if(val) $('#st-selected-text-input').val(val);
+        }
+
+    });
+
+}
+
+/**
+ * Loads a tweet and the classifier choosen category for that tweet
+ * 
+ * @returns {Promise}
+ */
+const loadNewGuidedTrainingTweet = () => {
+
+    return new Promise((resolve, reject) => {
+
+        // Gets the tweet
+        Tweets.pickOne().then((tweet) => {
+
+            // Gets the classification for the tweet
+            fetch('/api/classifier/categorize', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ text: tweet.text })
+            })
+            .then(async (response) => {
+
+                // Parses the response as a JSON
+                let data = await response.json();
+                // Verifies if the request has been completed succesfully
+                if(response.status >= 300) return reject(new Error(data.message));
+
+                resolve({...data, tweet });
+        
+            })
+            .catch((error) => reject(error));
+
+        })
+        .catch((error) => reject(error));
+
+    })
+
+    
+
+    
+
+    
+
+    /*
+    // Sets the interface loading
+    $('#gt-row').append(`
+        <div id="gt-loading-div" class="col-12 d-flex justify-content-center">
+            <div class="spinner-border text-primary" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    `);
+
+    */
+
+}
+
+/**
+ * Initializes the content of the Guided Training Tab
+ */
+const initGuidedTraining = () => {
+
+    // Removes the click listener which is going to be resetted below
+    $('#gt-skip-tweet-classification-button').off('click');
+
+    // Sets the skip tweet classification button functionality
+    $('#gt-skip-tweet-classification-button').click(function(event) {
+
+        // Sets the UI Loading
+        $(this).attr('disabled', true);
+        $(this).html("<span class=\"spinner-border spinner-border-sm\" role=\"status\" aria-hidden=\"true\"></span>");
+
+        // Loads the tweet and the classification
+        loadNewGuidedTrainingTweet()
+            .then(({ classifications, tweet }) => {
+
+                // Creates and insert into the dom the tweet card
+                displayTweet('gt-tweet-container', tweet);
+                // Sets the classifier choosen category data
+                $('#gt-classification-tweet-category').html(classifications[0].label);
+                let percentage = $('#gt-classification-tweet-percentage');
+                let pValue = Math.round(classifications[0].value * 100);
+                percentage.attr('style', `width: ${pValue}%`);
+                percentage.attr('aria-valuenow', pValue);
+                percentage.html(pValue + "%");
+
+                // Shows the row
+                $('#gt-row').removeClass('d-none');
+
+            })
+            // Notifies the user of the error
+            .catch((error) => toastr.error(error.message))
+            // Sets the skip button to be usable
+            .finally(() => {
+                $(this).removeAttr('disabled');
+                $(this).html("<i class=\"fas fa-forward\"></i> Skip");
+            });
+
+    });
+
+    // Loads the tweet data
+    $('#gt-skip-tweet-classification-button').trigger('click');
+
+}
+
+/**
+ * App Logic
+ */
+
 // Wait till the document is fully loaded
 $(function() {
 
@@ -120,79 +232,13 @@ $(function() {
     // Enabling toasts close button
     toastr.options.closeButton = true;
 
-    Tweets.pickOne()
-        .then((tweet) => displayTweet('st-tweet-container', tweet))
-        .catch((error) => toastr.error(error.message))
-        .finally(() => hideAppLoadingScreen())
+    // Initializes the Simple Training Tab content
+    initSimpleTraining();
 
-    // Sets the skip tweet classification button functionality
-    const skipTweet = $('#st-skip-tweet-classification-button');
-    skipTweet.click(() => {
-
-        // Disabling the button while it loads the next tweet
-        skipTweet.attr('disabled', 'true');
-        skipTweet.html("<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>");
-
-        // Gets the tweet
-        Tweets.pickOne()
-            // Sets the tweet into the page
-            .then((tweet) => displayTweet('st-tweet-container', tweet))
-            // Tost the error
-            .catch((error) => toastr.error(error.message))
-            // Reset the skip button to be usable
-            .finally(() => {
-                skipTweet.removeAttr('disabled');
-                skipTweet.html("<i class=\"fas fa-forward\"></i> Skip");
-                $("textarea[name=st-selected-text]").val('')
-            })
-
+    // Sets a click listener on the Guided Training tab which initializes the tab content
+    $('#pills-guided-training-tab').click(() => {
+        // Initializes the Guided Training Tab content
+        initGuidedTraining();
     })
-
-    // Sets the classification form behavior
-    $('#st-classification-form').on('submit', (event) => {
-
-        // Prevents the form to be submitted in the "standard" way
-        event.preventDefault();
-
-        // Sets the submit button on loading
-        let classificationSubmitButton = $('#st-classification-form-submit-button')
-        classificationSubmitButton.attr('disabled', 'true');
-        classificationSubmitButton.html("<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>");
-
-        // Sends the request to the
-        fetch('/api/classifier/learn', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ text: $("textarea[name=st-selected-text]").val(), category: $("input[name=st-radio-classification]").val() })
-        })
-        // Computes the response
-        .then(async (response) => {
-            let data = await response.json();
-            if(response.status >= 300) return toastr.error(data.message);
-            toastr.success(data.message);
-            skipTweet.trigger('click');
-            $('#st-classification-form').trigger('reset');
-        })
-        // Error Handling
-        .catch((error) => toastr.error("Unknown error."))
-        .finally(() => {
-            classificationSubmitButton.removeAttr('disabled');
-            classificationSubmitButton.html("Submit");
-        })
-
-    })
-
-    document.addEventListener('selectionchange', () => {
-
-        // Sets the tweet selected text into the selected text input
-        if($(window.getSelection().anchorNode.parentElement).attr('id') === 'st-tweet-text') {
-            let val = window.getSelection().toString();
-            if(val) $('#st-selected-text-input').val(val);
-        }
-            
-
-    });
-        
-    
 
 })
---
+++ public/scripts/tweets.js
@@ -0,0 +1,83 @@
+/**
+ * OVERVIEW: Tweets Class provides useful methods to iteract with the tweets api
+ */
+class Tweets {
+    
+    /**
+     * Checks if there are some cached tweets and checks the data integrity
+     * 
+     * @returns {Boolean}
+     */
+    static #areCached = () => {
+
+        // Check if cache contains tweets list data
+        let tweets = localStorage.getItem('tweets');
+
+        // Checks the integrity of the data
+        try {
+            var tweetsArray = JSON.parse(tweets);
+        }
+        catch(error) {
+            return false;
+        }
+        if(!tweetsArray || tweetsArray.some(({ created_at, text, author_id, id }) => {
+            return typeof(created_at) !== typeof(text) || typeof(text) !== typeof(author_id) || typeof(author_id) !== typeof(id) || typeof(id) !== 'string'
+        })) return false;
+
+        return true;
+
+    }
+
+    /**
+     * Store into the cache the array of tweets
+     * 
+     * @param {Array} tweets
+     */
+    static #cache = (tweets) => localStorage.setItem('tweets', JSON.stringify(tweets))
+
+    /**
+     * Returns an array of tweets which can come from the cache or from the api
+     * 
+     * @returns {Array}
+     */
+    static #getTweets = async () => {
+
+        return new Promise((resolve, reject) => {
+
+            // Checks if there are at least one tweet cached, in that case it returns the cached tweets
+            if(Tweets.#areCached() && JSON.parse(localStorage.getItem('tweets')).length > 0) 
+                return resolve({ tweets: JSON.parse(localStorage.getItem('tweets')), source: 'cache' })
+    
+            // No tweets cached, retrieves them from the api
+            fetch('/api/tweets')
+                // Converts the response into a JSON object
+                .then((response) => response.json())
+                .then((data) => {
+                    // Returns the tweets list
+                    resolve({ tweets: data, source: 'api' })
+                })
+                // Returns the error
+                .catch((error) => reject(error));
+            
+        });
+
+    }
+
+    /**
+     * Returns the first tweet available and updates the cache removing the returned tweet
+     * 
+     * @returns {Object}
+     */
+    static pickOne = async () => {
+
+        let tweets = (await Tweets.#getTweets()).tweets
+
+        // Takes the tweets and caches the other ones
+        let tweet = tweets[0]
+        Tweets.#cache(tweets.slice(1))
+
+        return tweet
+        
+    }
+
+}
